0cad70e836fc323bb59dc3a0393999d16f7f5f69
raboy1997 <maks.rublya@mail.ru>
2017-03-03 18:11:08 +0200

    create registration and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root "static_pages#index"
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signout',  to: 'sessions#destroy', via: :delete
+
+  post 'users/new' , to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
